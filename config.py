33f3cc99197086d7125287d933c223d45405e060
Nelinner <denisrabov2011@gmail.com>
2026-05-16 17:01:55 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+BOT_TOKEN = "8254209430:AAEGZGjdnzSXFUudGjF9MOydkU7s8QyDR28"
+CHANNEL_ID = "@hp404faceit"          # канал, на который надо подписаться
+ADMIN_IDS = [6394456595]    # Telegram ID администраторов
+DATABASE_URL = "sqlite+aiosqlite:///bot.db"
